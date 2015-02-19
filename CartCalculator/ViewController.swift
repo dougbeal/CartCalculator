eec264197ee9aad6cc66707c488b7d8093993578
Douglas Beal <git@dougbeal.com>
2015-02-18 17:34:12 -0800

    Basic Cartography, build settings not fixed yet

--- CartCalculator/ViewController.swift
+++ CartCalculator/ViewController.swift
@@ -7,12 +7,16 @@
 //
 
 import UIKit
+import Cartography
 
 class ViewController: UIViewController {
 
+    var excludeStatusBarView: UIView?
+    var layoutElements: [String: View]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        createViews()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // Allow any orinetation.
+    // How to set the orientation. The return value is not what we expect, Int not UInt so we cast.
+    override func supportedInterfaceOrientations() -> Int {
+        return Int(UIInterfaceOrientationMask.All.rawValue)
+    }
+
+    // status bar may disappear when rotation happens, so update excludeStatusBarView size 
+    override func viewWillLayoutSubviews() {
+        excludeStatusBarView!.frame = UIScreen.mainScreen().applicationFrame
+    }
+
+    func createViews() {
+        excludeStatusBarView = UIView(frame: UIScreen.mainScreen().applicationFrame)
+        excludeStatusBarView!.accessibilityIdentifier = "exclude status bar view"
+        layoutElements["view"] = view
+        layoutElements["excludeStatusBarView"] = excludeStatusBarView
+        
+        view.addSubview(excludeStatusBarView!)
+        layout(layoutElements) { (le: [String: View]) in
+                                 println("\(le)")
+        }
+    }
 
 }
 
