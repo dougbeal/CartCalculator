a27512fad229bc618720775fa71696a722963197
Douglas Beal <git@dougbeal.com>
2015-02-18 16:33:47 -0800

    Initial Commit

---
+++ CartCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CartCalculator
+//
+//  Created by Douglas Beal on 2/18/15.
+//  Copyright (c) 2015 Murder Of Crows Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
